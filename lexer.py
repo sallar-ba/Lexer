1409c42e4d11f99ad2972ac78aa7420860ed06c2
Sallar <106476322+sallar-ba@users.noreply.github.com>
2023-05-31 23:23:27 +0500

    Added Forms.py     Modified the Lexer.py for the forms     Updated Lexer.html, added form tag and updated the jinja format

--- lexer.py
+++ lexer.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from forms import CodeForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '9f5ac35a1742f4f096316546'
 
 @app.route('/')
 @app.route('/index')
@@ -12,9 +14,14 @@ def index():
 def team():
     return render_template('team.html')
 
-@app.route('/lexer')
+@app.route('/lexer', methods=['GET', 'POST'])
 def lexer():
-    return render_template('lexer.html')
+    form = CodeForm()
+    if form.validate_on_submit():
+        code = form.Code.data
+        # Function Call For Lexical and Syntax Analyzer
+
+    return render_template('lexer.html', form=form)
 
 
 @app.route('/tree')
