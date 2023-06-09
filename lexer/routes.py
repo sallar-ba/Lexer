47fe6e5bddb92e389e384eff8224246117fbdcf8
Sallar <106476322+sallar-ba@users.noreply.github.com>
2023-06-09 16:21:14 +0500

    Updated Routes.py, and Output-Lexer.py

--- lexer/routes.py
+++ lexer/routes.py
@@ -2,7 +2,7 @@ from lexer import app
 from flask import render_template
 from .forms import CodeForm
 from .scripts.lexical import tokenize
-from .scripts.syntax import foo1
+from .scripts.syntax import Foo
 
 @app.route('/')
 @app.route('/index')
@@ -22,12 +22,10 @@ def lexer():
     if form.validate_on_submit():
         code = form.code.data
         tokens = tokenize(code)
-        isValid = foo1(tokens)
-        return render_template('output_lexer.html', form=form, tokens=tokens, isValid=isValid)
+        Valid = Foo(tokens)
+        return render_template('output_lexer.html', form=form, tokens=tokens, Valid=Valid)
      
-    return render_template('lexer.html', form=form, tokens=None, isValid=None)
-
-
+    return render_template('lexer.html', form=form, tokens=None, Valid=None)
 
 @app.route('/tree', methods=['GET', 'POST'])
 def tree():
