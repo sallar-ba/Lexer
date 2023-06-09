4eb5d945c5297299cf84d96d546625e42dea9cbb
Mohammad Ali <m.ali39121@gmail.com>
2023-06-09 09:17:15 +0500

    Updated lexical.py script, added new entries to the patterns.

--- lexer/scripts/lexical.py
+++ lexer/scripts/lexical.py
@@ -8,26 +8,37 @@ patterns = [
     ('WHILE', r'while'),  # WHILE
     ('FOR', r'for'),  # FOR
     ('SWITCH', r'switch'),  # SWITCH
-    ('CASE', r'case'),  # SWITCH
+    ('CASE', r'case'),  # CASE
+    ('EXIT', r'exit'),  # exit
+    ('BREAK', r'break'),  # BREAK
+    ('CONTINUE', r'continue'),  # CONTINUE
     ('IDENTIFIER', r'[a-zA-Z_][a-zA-Z0-9_]*'),  # Alphabets and underscore followed by alphanumeric characters
     ('NUMBER', r'\d+'),  # Numbers
     ('STRING', r'\"([^\\\n]|(\\.))*?\"'),  # Strings within double quotes
     ('INCREMENT', r'\+\+'),  # Increment operator
     ('DECREMENT', r'--'),  # Decrement operator
+    ('GREATER_THAN_EQUAL', r'<='),  # Less than equal operator
+    ('LESS_THAN_EQUAL', r'>='),  # Greater than equal operator
+    ('NOT_EQUAL', r'!='),  # Not equal operator
+    ('MINUS_EQUALS', r'-='),  # Less than equal operator
+    ('PLUS_EQUALS', r'\+='),  # Greater than equal operator
+    ('MULTIPLY_EQUALS', r'\*='),  # Not equal operator
+    ('DIVIDE_EQUALS', r'/='),  # Not equal operator 
+    ('MOD_EQUALS', r'%='),  # Not equal operator 
     ('PLUS', r'\+'),  # Plus operator
     ('MINUS', r'-'),  # Minus operator
     ('MULTIPLY', r'\*'),  # Multiply operator
     ('DIVIDE', r'/'),  # Divide operator
     ('MODULUS', r'%'),  # Modulus operator
-    ('EQUALS', r'='),  # Equal to operator
+    ('EQUAL_TO', r'=='),  # Equal to operator
     ('SEMICOLON', r';'),  # Semicolon
     ('COLON', r':'),  # Colon
-    ('LPAREN', r'\('),  # Left parenthesis
-    ('RPAREN', r'\)'),  # Right parenthesis
-    ('LBRACE', r'{'),  # Left brace
-    ('RBRACE', r'}'),  # Right brace
-    ('LBRACKET', r'\['),  # Left bracket
-    ( 'RBRACKET', r'\]'),  # Right bracket
+    ('LEFT_ROUND_BRACKET', r'\('),  # Left parenthesis
+    ('RIGHT_ROUND_BRACKET', r'\)'),  # Right parenthesis
+    ('LEFT_CURLY_BRACKET', r'{'),  # Left brace
+    ('RIGHT_CURLY_BRACKET', r'}'),  # Right brace
+    ('LEFT_SQUARE_BRACKET', r'\['),  # Left bracket
+    ('RIGHT_SQUARE_BRACKET', r'\]'),  # Right bracket
     ('ASSIGN', r'='),  # Assignment operator
     ('OR', r'\|\|'),  # Logical OR operator
     ('AND', r'&&'),  # Logical AND operator
@@ -37,16 +48,17 @@ patterns = [
     ('GREATER_THAN', r'>'),  # Greater than operator
     ('HASH', r'#'),  # Hash
     ('DOT', r'\.'),  # Dot
+    ('NOT', r'!'),  # Logical AND operator
 ]
 
 # List of reserved words in C++
 reserved_words = [
     'alignas', 'alignof', 'and', 'and_eq', 'asm', 'atomic_cancel', 'atomic_commit', 'atomic_noexcept',
-    'auto', 'bitand', 'bitor', 'bool', 'break', 'catch', 'char', 'char8_t', 'char16_t', 'char32_t',
-    'class', 'compl', 'concept', 'const', 'consteval', 'constexpr', 'constinit', 'const_cast', 'continue',
+    'auto', 'bitand', 'bitor', 'bool', 'catch', 'char', 'char8_t', 'char16_t', 'char32_t',
+    'class', 'compl', 'concept', 'const', 'consteval', 'constexpr', 'constinit', 'const_cast',
     'co_await', 'co_return', 'co_yield', 'decltype', 'default', 'delete', 'double', 'dynamic_cast',
     'enum', 'explicit', 'export', 'extern', 'false', 'float', 'friend', 'goto', 'iostream', 
-    'include', 'inline', 'int', 'long', 'main', 'mutable', 'namespace', 'new', 'noexcept', 'not', 'not_eq', 'nullptr', 
+    'include', 'inline', 'int', 'long', 'main', 'mutable', 'namespace', 'new', 'noexcept', 'not_eq', 'nullptr', 
     'operator', 'or', 'or_eq', 'private', 'protected', 'public', 'reflexpr', 'register', 'reinterpret_cast', 
     'requires', 'return', 'short', 'signed', 'sizeof', 'static', 'static_assert', 'static_cast', 'std', 'struct', 
     'synchronized', 'template', 'this', 'thread_local', 'throw', 'true', 'try', 'typedef', 'typeid', 
